7a060a3182b52d3bdef76ad51345153613d3e2f0
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-10-27 11:57:55 +1100

    Adding Final Solution

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+
+app.post('/',(req,res)=>{
+
+  const {payload} = req.body;
+
+  if (payload)
+      {
+        const filteredShows = payload.filter(show=>{
+          return show.drm && show.episodeCount > 0
+        })
+        const showFilteredFields = filteredShows.map(show=>{
+          return {image: show.image.showImage,slug: show.slug,title: show.title}
+        })
+        res.send({response: showFilteredFields})
+      }
+  else
+      {
+        res.status(400).send({error:'Could not decode request: JSON parsing failed'})
+      }
+  
+})
+app.listen(process.env.PORT||3000,()=>console.log('Listening on Port 3000'));
